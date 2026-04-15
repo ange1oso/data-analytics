3da24a975e27134f68eef58152f14ff03770aa20
angel osorio <aosorio@my.yearupunited.org>
2026-04-15 12:17:11 -0500

    Added quieries of actor and film tables

--- week-02/Ex1B_First_Query.sql
+++ week-02/Ex1B_First_Query.sql
@@ -1,3 +1,9 @@
 /*
 Sakila is a sample database simulating a real world business problem that is used for practice
 */
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved 200 records
+
+SELECT * FROM film; -- Retrieved 1000 records
+
